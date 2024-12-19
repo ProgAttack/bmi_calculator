57e1e143ef6614b601d6d3c1f0d92057d18fb152
Giancarlo Kramer <92767138+ProgAttack@users.noreply.github.com>
2024-12-19 16:25:19 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:bmi_calculator/icon_content.dart';
+import 'package:bmi_calculator/reusable_card.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
+const bottomContainerColor = Color(0xFFEB1555);
+enum GenderType {male, female}
+
+
+void main() => runApp(const BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: const Color(0xFF0A0E21),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      ),
+      home: const InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Color maleCardColour = inactiveCardColour;
+  Color femaleCardColour = inactiveCardColour;
+
+   GenderType? selectGender;
+
+  /*//1 == male, 2 == female
+  void updateColour(GenderType gender) {
+    if (gender == GenderType.male)  {
+      if (maleCardColour == inactiveCardColour) {
+        maleCardColour = activeCardColour;
+        femaleCardColour = inactiveCardColour;
+      } else {
+        maleCardColour = inactiveCardColour;
+      }
+    }
+    if (gender == GenderType.female) {
+      if (femaleCardColour == inactiveCardColour) {
+        femaleCardColour = activeCardColour;
+        maleCardColour = inactiveCardColour;
+      } else {
+        femaleCardColour = inactiveCardColour;
+      }
+    }
+  }
+*/
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    onPress: (){
+                      setState(() {
+                        selectGender = GenderType.male;
+                      });
+                    },
+                    colour: selectGender == GenderType.male ? activeCardColour : inactiveCardColour,
+                    cardChild:  const IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: "MALE",
+                    ),                   
+                  ), //Refatorado
+                ),
+                Expanded(
+                  child: ReusableCard(
+                      onPress: (){
+                        setState(() {
+                          selectGender = GenderType.female;
+                        });
+                      },
+                      colour: selectGender == GenderType.female ? activeCardColour : inactiveCardColour,
+                      cardChild: const IconContent(
+                          label: "FEMALE", icon: FontAwesomeIcons.venus)),
+                ),
+              ],
+            )),
+            const Expanded(
+                child: ReusableCard(colour: activeCardColour)), //meio
+            const Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: activeCardColour),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: activeCardColour),
+                ),
+              ],
+            )),
+            Container(
+              color: bottomContainerColor,
+              margin: const EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
+          ],
+        ));
+  }
+}
